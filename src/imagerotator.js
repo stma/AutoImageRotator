6cfe9230978e1782a31c169d3e408ccb06fcdbe3
matyi <matyi@miutcank.hu>
2018-10-22 11:14:27 +0200

    fix promise return

--- src/imagerotator.js
+++ src/imagerotator.js
@@ -4,56 +4,58 @@ import base64js from 'base64-js';
 
 const _lookOver = debug('ImageRotator');
 
-const _resetOrientation = (srcBase64, srcOrientation) => (resolve, reject) => {
-    const img = new Image();
+const _resetOrientation = (srcBase64, srcOrientation) => new Promise(
+    (resolve, reject) => {
+        const img = new Image();
 
-    img.onload = function() {
-        const width = img.width;
-        const height = img.height;
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
+        img.onload = function() {
+            const width = img.width;
+            const height = img.height;
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
 
-        if (4 < srcOrientation && srcOrientation < 9) {
-            canvas.width = height;
-            canvas.height = width;
-        } else {
-            canvas.width = width;
-            canvas.height = height;
-        }
+            if (4 < srcOrientation && srcOrientation < 9) {
+                canvas.width = height;
+                canvas.height = width;
+            } else {
+                canvas.width = width;
+                canvas.height = height;
+            }
 
-        switch (srcOrientation) {
-        case 2:
-            ctx.transform(-1, 0, 0, 1, width, 0);
-            break;
-        case 3:
-            ctx.transform(-1, 0, 0, -1, width, height );
-            break;
-        case 4:
-            ctx.transform(1, 0, 0, -1, 0, height );
-            break;
-        case 5:
-            ctx.transform(0, 1, 1, 0, 0, 0);
-            break;
-        case 6:
-            ctx.transform(0, 1, -1, 0, height, 0);
-            break;
-        case 7:
-            ctx.transform(0, -1, -1, 0, height, width);
-            break;
-        case 8:
-            ctx.transform(0, -1, 1, 0, 0, width);
-            break;
-        default:
-            break;
-        }
+            switch (srcOrientation) {
+            case 2:
+                ctx.transform(-1, 0, 0, 1, width, 0);
+                break;
+            case 3:
+                ctx.transform(-1, 0, 0, -1, width, height );
+                break;
+            case 4:
+                ctx.transform(1, 0, 0, -1, 0, height );
+                break;
+            case 5:
+                ctx.transform(0, 1, 1, 0, 0, 0);
+                break;
+            case 6:
+                ctx.transform(0, 1, -1, 0, height, 0);
+                break;
+            case 7:
+                ctx.transform(0, -1, -1, 0, height, width);
+                break;
+            case 8:
+                ctx.transform(0, -1, 1, 0, 0, width);
+                break;
+            default:
+                break;
+            }
 
-        ctx.drawImage(img, 0, 0);
+            ctx.drawImage(img, 0, 0);
 
-        resolve(canvas.toDataURL());
-    };
+            resolve(canvas.toDataURL());
+        };
 
-    img.src = srcBase64;
-};
+        img.src = srcBase64;
+    }
+);
 
 const _getOrientation = (file) => (resolve, reject) => {
     const reader = new FileReader();
